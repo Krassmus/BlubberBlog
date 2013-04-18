0c3742d55cbf9c9a0489dcfe386c327fb22c48af
Rasmus Fuhse <Krassmus@gmail.com>
2013-04-18 19:39:58 +0200

    init plugin

---
+++ BlubberBlog.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class BlubberBlog extends StudIPPlugin implements SystemPlugin {
+    public function __construct() {
+        parent::__construct();
+        $top = new Navigation();
+        $top->setTitle(_("Blog"));
+        $top->setURL(URLHelper::getURL("plugins.php/blubber/streams/profile?user_id=8a2ca05a506a147e9f99a333e2bc3cd7"));
+        $top->setImage($this->getPluginURL()."/assets/news_header.png");
+        Navigation::addItem("/blubberblog", $top);
+    }
+}
