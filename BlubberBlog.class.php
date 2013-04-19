a0bfb61e6bec8278815be40d93d8b134f9f7bb84
Rasmus Fuhse <Krassmus@gmail.com>
2013-04-19 12:47:07 +0200

    rename file

--- BlubberBlog.class.php
+++ BlubberBlog.class.php
@@ -5,7 +5,7 @@ class BlubberBlog extends StudIPPlugin implements SystemPlugin {
         parent::__construct();
         $top = new Navigation(_("Blog"));
         $top->setURL(URLHelper::getURL("plugins.php/blubber/streams/profile?user_id=8a2ca05a506a147e9f99a333e2bc3cd7"));
-        $top->setImage($this->getPluginURL()."/assets/news2.png");
+        $top->setImage($this->getPluginURL()."/assets/news_header.png");
         Navigation::addItem("/blubberblog", $top);
     }
 }
