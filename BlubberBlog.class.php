38f9c2abc9f1cb600d97c09201108415bfb479fc
Rasmus Fuhse <krasmus@gmail.com>
2013-10-28 22:30:07 +0100

    retina icon

--- BlubberBlog.class.php
+++ BlubberBlog.class.php
@@ -5,7 +5,11 @@ class BlubberBlog extends StudIPPlugin implements SystemPlugin {
         parent::__construct();
         $top = new Navigation(_("Blog"));
         $top->setURL(URLHelper::getURL("plugins.php/blubber/streams/profile?user_id=8a2ca05a506a147e9f99a333e2bc3cd7"));
-        $top->setImage($this->getPluginURL()."/assets/news_header.png");
+        if ($GLOBALS['auth']->auth['devicePixelRatio'] > 1.2) {
+            $top->setImage($this->getPluginURL()."/assets/news_header@2x.png");
+        } else {
+            $top->setImage($this->getPluginURL()."/assets/news_header.png");
+        }
         Navigation::addItem("/blubberblog", $top);
         
         PageLayout::addHeadElement("link", array('rel' => "stylesheet", "href" => $this->getPluginURL()."/assets/blubberblog.css"));
